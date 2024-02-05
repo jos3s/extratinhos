91d51f419a988329ab66e910812af4877c948de6
José Ulisses <julissesmacedo@outlook.com>
2024-02-05 17:10:23 -0300

    add: entry request dto

---
+++ extratinhos.api/DTOs/EntryRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace extratinhos.DTOs
+{
+	public class EntryRequest
+	{
+		[JsonPropertyName("valor")]
+		public long Value { get; set; }
+
+		[JsonPropertyName("tipo")]
+		[StringLength(1, MinimumLength = 1, ErrorMessage = "The Gender must be 1 characters.")]
+		[RegularExpression("c|d", ErrorMessage = "The Gender must be either 'M' or 'F' only.")]
+		public string Type { get; set; }
+
+		[JsonPropertyName("descricao")]
+		public string Descrition { get; set; }
+	}
+}
