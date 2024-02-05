16d9d4615cb12dbb99e3460f36d5f3395b7dd785
José Ulisses <julissesmacedo@outlook.com>
2024-02-05 19:17:55 -0300

    feat: create repositoreis to entities

---
+++ extratinhos.api/Repositories/ClientRepository.cs
@@ -0,0 +1,41 @@
+﻿using extratinhos.Datasource;
+using extratinhos.Entitys;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace extratinhos.api.Repositories;
+
+public class ClientRepository : IRepository<Client>
+{
+    private AppDbContext _context;
+
+    public ClientRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public List<Client> GetAll() => _context.Clients.AsNoTracking().ToList();
+
+    public Client? GetById(long id) => _context.Clients.FirstOrDefault(c => c.Id == id);
+
+    public IRepository<Client> Insert(Client entity)
+    {
+        _context.Clients.Add(entity);
+        return this;
+    }
+
+    public IRepository<Client> InsertRange(List<Client> entitys)
+    {
+        _context.Clients.AddRange(entitys);
+        return this;
+    }
+
+    public void Save() => _context.SaveChanges();
+
+    public IRepository<Client> Update(Client entity)
+    {
+        entity.UpdatedAt = DateTime.Now;
+        _context.Clients.Update(entity);
+        return this;
+    }
+}
